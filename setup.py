31d70d54c311779ec24c25b812789b1edf6ab803
Jonathan Hartley <Jonathan.Hartley@ibm.com>
2017-11-29 08:51:14 -0600

    Fix documentation.

--- setup.py
+++ setup.py
@@ -66,9 +66,9 @@ def get_sdist_config():
         # http://pypi.python.org/pypi?:action=list_classifiers
         classifiers=[
             #'Development Status :: 1 - Planning',
-            #'Development Status :: 2 - Pre-Alpha',
+            'Development Status :: 2 - Pre-Alpha',
             #'Development Status :: 3 - Alpha',
-            'Development Status :: 4 - Beta',
+            #'Development Status :: 4 - Beta',
             #'Development Status :: 5 - Production/Stable',
             #'Development Status :: 6 - Mature',
             #'Development Status :: 7 - Inactive',
